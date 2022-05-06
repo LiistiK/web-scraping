d7643b4ee75b726200402efb7331fb87028398c3
Гамид <95250214+LiistiK@users.noreply.github.com>
2022-05-06 21:57:25 +0300

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -9,7 +9,9 @@ text = ret.text
 
 soup = bs4.BeautifulSoup(text, features='html.parser')
 articles = soup.find_all('article')
+
 for article in articles:
+    data = article.find(class_='tm-article-snippet__datetime-published').time['title']
     hubs = article.find_all(class_='tm-article-snippet__hubs-item')
     hubs = set(hub.text.strip() for hub in hubs)
     for hub in hubs:
@@ -17,5 +19,5 @@ for article in articles:
             href = article.find(class_='tm-article-snippet__title-link').attrs['href']
             link = url + href
             title = article.find('h2').find('span').text
-            result = f"Статья - {title} доступна по ссылке {link}"
+            result = f"{data} - {title} - {link}"
             print(result)
